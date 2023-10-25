a63e26556565f994e6aa7658a064ccfe99e45a1e
shrikkkk <xiayuminleah@outlook.com>
2023-10-25 19:37:35 +0800

    init

---
+++ MyComponent/MyComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyComponentApp.swift
+//  MyComponent
+//
+//  Created by Yumin Xia on 2023/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MyComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
